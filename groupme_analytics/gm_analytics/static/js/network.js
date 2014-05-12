3e1fc40290a979cdafcd8179e157912e8f8f7a4b
Patrick McGannon <pmcgannon22@gmail.com>
2014-05-12 18:40:52 -0500

    Layout for network looks good to go.

--- groupme_analytics/gm_analytics/static/js/network.js
+++ groupme_analytics/gm_analytics/static/js/network.js
@@ -5,10 +5,20 @@ var w = 960,
 var color = d3.scale.category20();
 
 var force = d3.layout.force()
-.charge(-10000)
-.linkDistance(325)
-.gravity(.7)
-.size([w, h]);
+    .charge(function(d) { return d.weight; })
+//    .linkDistance(325)
+    .gravity(0)
+    .size([w, h]);
+
+var zones = [5, 2]; //[horizontal,vertical] division
+var foci = [];
+for(var a=1;a<=zones[0];a++) {
+    for(var b=1;b<=zones[1];b++) {
+        var xf = ((w/zones[0])*(2*a - 1))/2;
+        var yf = ((h/zones[1])*(2*b-1))/2;
+        foci.push({x: xf, y: yf});
+    }
+}
 
 var svg = d3.select("#network").append("svg")
 .attr("width", w)
@@ -17,103 +27,127 @@ var svg = d3.select("#network").append("svg")
 .attr("preserveAspectRatio", "xMinYMin meet");
 
 d3.json("/group/4234424/graph", function(error, graph) {
-    console.log(graph);
     force.nodes(graph.nodes)
-      .links(graph.links)
-      .start();
+      .links(graph.links);
 
     svg.append("defs").append("marker")
-      .attr("id", "arrowhead")
-      .attr("refX", 1)
-      .attr("viewBox", "0 0 10 10")
-      .attr("refY", 5)
-      .attr("markerWidth", 6)
-      .attr("markerHeight", 6)
-      .attr("orient", "auto")
-      .append("path")
-      .attr("d", "M 0 0 L 10 5 L 0 10 z");
+    .attr("id", "arrowhead")
+    .attr("refX", 1)
+    .attr("viewBox", "0 0 10 10")
+    .attr("refY", 5)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("d", "M 0 0 L 10 5 L 0 10 z");
 
-  var link = svg.selectAll(".link-wrap")
-      .data(graph.links)
-    .enter().append("g")
-      .attr("class", "link-wrap")
-      .append("path")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); })
-      .attr("marker-end", "url(#arrowhead)");
+  force.on("tick", function(e) {
+      var k = ~~(100  * Math.random());
 
-  var node = svg.selectAll(".node")
-      .data(graph.nodes)
-      .enter().append("image")
-      .attr("class", "node")
-      .attr("x", -r/2)
-      .attr("y", -r/2)
-      .attr("width", r)
-      .attr("height", r)
-      .on("mouseover", fade(.1)).on("mouseout", fade(1));
+  // Push nodes toward their designated focus.
+      graph.nodes.forEach(function(o, i) {
+        var foc  = parseInt(o.id) % (zones[0] * zones[1]);
 
-    var linkedByIndex = {};
-    graph.links.forEach(function(d) {
-      linkedByIndex[d.source.index + "," + d.target.index] = 1;
-    });
-
-function neighboring(a, b) {
-    if(linkedByIndex[a.index + "," + b.index]){
-        return 1;
-    } else if(linkedByIndex[b.index + "," + a.index]) {
-        return -1;
-    } else {
-        return 0;
-    }
-}
-
-function fade(opacity) {
-    return function(d) {
-        link.style("stroke-opacity", function(o) {
-            return o.source === d || o.target === d ? 1 : opacity;
-        }).style("stroke", function(o) {
-            if(opacity < 1) {
-                if(o.source === d) return "#FFCC6C"; //Out Edge Color
-                else if(o.target === d) return "#00CDCD"; //In Edge Color
-                else return "";
-            }
-            return "";
-        }).style("stroke-width", function(o) {
-            if(opacity < 1) {
-                if(o.source === d || o.target === d) {
-                    d3.select(this.nextSibling).style("display","block");
-                    return "3px";
-                }
-            }
-            d3.select(this.nextSibling).style("display","");
-            return "";
-        });
-
-    };
-}
-
-  node.append("title")
-      .text(function(d) { return d.name; });
-  force.on("tick", function() {
-      node.attr("cx", function(d) { return d.x = Math.max(r, Math.min(w - r, d.x)); })
-          .attr("cy", function(d) { return d.y = Math.max(r, Math.min(h - r, d.y)); });
-
-        link.attr("d", function(d) {
-            var dx = d.target.x - d.source.x,
-                dy = d.target.y - d.source.y,
-                dr = Math.sqrt(dx * dx + dy * dy);
-            return "M" + d.source.x + "," + d.source.y + " A" + dr + "," + dr + " 0 0, 1 " +
-                    d.target.x + "," + d.target.y;
-        });
+        var yy = (Math.random() < 0.5 ? -1 : 1) * k + foci[foc].y;
+        var xx = (Math.random() < 0.5 ? -1 : 1) * k + foci[foc].x;
+        if(xx > r && xx < (w-r))
+            o.x = xx;
+        if(yy > r && yy < (h-r))
+            o.y = yy;
+      });
   });
 
-  force.on("end", function() {
-          svg.selectAll(".link-wrap").each(function(d, i) {
+  var link, node;
+  setTimeout(function() {
+        force.start();
+        for (var i = 100; i > 0; --i) force.tick();
+        force.stop();
+
+        link = svg.selectAll(".link-wrap")
+            .data(graph.links)
+            .enter().append("g")
+            .attr("class", "link-wrap")
+            .append("path")
+            .attr("class", "link")
+            .style("stroke-width", function(d) { return Math.sqrt(d.value); })
+            .attr("marker-end", "url(#arrowhead)");
+
+        node = svg.selectAll(".node")
+            .data(graph.nodes)
+            .enter().append("g")
+            .attr("class", "node")
+            .on("mouseover", fade(.1)).on("mouseout", fade(1));
+
+        graph.nodes.forEach(function(d, i) {
+          svg.select("defs").append("pattern")
+              .attr("id", "image-" + d.id)
+              .attr("x", 0).attr("y",0)
+              .attr("height", 40).attr("width", 40)
+              .append("image")
+              .attr("x",0).attr("y",0)
+              .attr("height", 40).attr("width", 40)
+              .attr("xlink:href", d.img);
+        });
+        node.append("circle")
+            .attr("xlink:href", function(d) { console.log(d); return d.img; })
+            .attr("class", "img-clip")
+            .attr("cx", 0)
+            .attr("cy", 0)
+            .attr("r", 20)
+            .attr("title", function(d) { return d.name; });
+
+        node.attr("transform", function(d) { return "translate(" + Math.max(r, Math.min(w - r, d.x))
+                                                      + "," + Math.max(r, Math.min(h - r, d.y)) + ")"; });
+        link.attr("d", function(d) {
+          var dx = d.target.x - d.source.x,
+              dy = d.target.y - d.source.y,
+              dr = Math.sqrt(dx * dx + dy * dy);
+          return "M" + d.source.x + "," + d.source.y + " A" + dr + "," + dr + " 0 0, 1 " +
+                  d.target.x + "," + d.target.y;
+        });
+
+        svg.selectAll(".link-wrap").each(function(d, i) {
               var t = d3.select(this);
               var n = t.select(".link").node();
               var point = n.getPointAtLength(n.getTotalLength()/2);
               t.append("text").attr("class", "edge-weight")
                   .text(d.weight).attr("x", point.x).attr("y", point.y);
         });
-  })
+
+        svg.selectAll(".img-clip")
+            .attr("fill", function(d) { return "url(#image-" + d.id + ")"; });
+        $("svg circle.img-clip").tooltip({
+            'container': 'body',
+            'placement': 'top'
+        });
+
+  }, 50);
+
+
+  function fade(opacity) {
+      return function(d) {
+          link.style("stroke-opacity", function(o) {
+              return o.source === d || o.target === d ? 1 : opacity;
+          }).style("stroke", function(o) {
+              if(opacity < 1) {
+                  if(o.source === d) return "#FFCC6C"; //Out Edge Color
+                  else if(o.target === d) return "#00CDCD"; //In Edge Color
+                  else return "";
+              }
+              return "";
+          }).style("stroke-width", function(o) {
+              if(opacity < 1) {
+                  if(o.source === d || o.target === d) {
+                      d3.select(this.nextSibling).style("display","block");
+                      return "3px";
+                  }
+              }
+              d3.select(this.nextSibling).style("display","");
+              return "";
+          });
+
+          var img_size = 15;
+          //resize image??
+      };
+  }
 });
