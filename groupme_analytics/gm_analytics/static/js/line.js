39c320b796012a81b4c219ba168555e5fcfe6e5e
Patrick McGannon <pmcgannon22@gmail.com>
2014-05-30 18:29:08 -0700

    Multiple lines on graph.

--- groupme_analytics/gm_analytics/static/js/line.js
+++ groupme_analytics/gm_analytics/static/js/line.js
@@ -30,100 +30,105 @@
         });
     };
 
+    var loader = Utils.renderLoader("#daily", "Loading...");
+
     d3.json("/group/"+group_id+"/dailydata?limit=35", function(error, data) {
-    color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
 
-    data.forEach(function(d) {
-        d.date = parseDate(d.date);
-    });
+        color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
 
-    var cats = color.domain().map(function(name) {
-        return {
-          name: name,
-          values: data.map(function(d) {
-            return {date: d.date, total: +d[name]};
-          })
-        };
-      });
-
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain([
-        d3.min(cats, function(c) { return d3.min(c.values, function(v) { return v.total; }); }),
-        d3.max(cats, function(c) { return d3.max(c.values, function(v) { return v.total; }); })
-      ]);
-
-    svg = d3.select("#daily").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    cats.map(function(c) { return c.values.sort(function(a,b) { return a.date - b.date; }); });
-
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("# of Messages");
-
-    var category = svg.selectAll(".category")
-      .data(cats).enter()
-      .append("g").attr("class", "category");
-
-    category.append("path")
-      .attr("class", "line")
-      .style("stroke",function(d) { return color(d.name); })
-      .attr("d", function(d) { return line(d.values); });
-
-    category.selectAll(".points")
-        .data(function(d) { return d.values; })
-        .enter().append("circle")
-        .attr("class", "point")
-        .attr("cx", function(d) { return x(d.date); })
-        .attr("cy", function(d) { return y(d.total); })
-        .attr("r", "3px")
-        .style("cursor", "pointer")
-        .style("fill", function(d, i, j) { return color(cats[j].name) })
-        .attr("title", function(d) {
-            return (d.date.getMonth()+1) + "/" + d.date.getDate() + "/" + d.date.getFullYear() + ": " + d.total;
-        }).on("click", function(d) {
-            var date = ('0' + (d.date.getMonth()+1)).slice(-2) + '/' + ('0' + d.date.getDate()).slice(-2) + '/' + d.date.getFullYear();
-            $("#conversationModal").modal({'show':true, 'remote':'messages/conversation?day='+date});
+        data.forEach(function(d) {
+            d.date = parseDate(d.date);
         });
 
-    $(".point").tooltip({
-        'container':'body',
-        'placement':'top'
-    });
+        loader.html('');
 
-    var legend = svg.selectAll(".legend")
-        .data(cats.map(function(d) { return d.name; }))
-        .enter().append("g")
-        .attr("class", "legend")
-        .attr("transform", function (d, i) { return "translate(-25," + (i * 20 + 50) + ")"; });
+        var cats = color.domain().map(function(name) {
+            return {
+              name: name,
+              values: data.map(function(d) {
+                return {date: d.date, total: +d[name]};
+              })
+            };
+          });
 
-    legend.append("rect")
-        .attr("x", width - 10)
-        .attr("width", 10)
-        .attr("height", 10)
-        .style("fill", color)
-        .style("stroke", "grey");
+        x.domain(d3.extent(data, function(d) { return d.date; }));
+        y.domain([
+            d3.min(cats, function(c) { return d3.min(c.values, function(v) { return v.total; }); }),
+            d3.max(cats, function(c) { return d3.max(c.values, function(v) { return v.total; }); })
+          ]);
 
-    legend.append("text")
-        .attr("x", width - 15)
-        .attr("y", 6)
-        .attr("dy", ".35em")
-        .style("text-anchor", "end")
-        .text(function (d) { return d; });
+        svg = d3.select("#daily").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        cats.map(function(c) { return c.values.sort(function(a,b) { return a.date - b.date; }); });
+
+        svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+        svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("# of Messages");
+
+        var category = svg.selectAll(".category")
+          .data(cats).enter()
+          .append("g").attr("class", "category");
+
+        category.append("path")
+          .attr("class", "line")
+          .style("stroke",function(d) { return color(d.name); })
+          .attr("d", function(d) { return line(d.values); });
+
+        category.selectAll(".points")
+            .data(function(d) { return d.values; })
+            .enter().append("circle")
+            .attr("class", "point")
+            .attr("cx", function(d) { return x(d.date); })
+            .attr("cy", function(d) { return y(d.total); })
+            .attr("r", "3px")
+            .style("cursor", "pointer")
+            .style("fill", function(d, i, j) { return color(cats[j].name) })
+            .attr("title", function(d) {
+                return (d.date.getMonth()+1) + "/" + d.date.getDate() + "/" + d.date.getFullYear() + ": " + d.total;
+            }).on("click", function(d) {
+                var date = ('0' + (d.date.getMonth()+1)).slice(-2) + '/' + ('0' + d.date.getDate()).slice(-2) + '/' + d.date.getFullYear();
+                $("#conversationModal").modal({'show':true, 'remote':'messages/conversation?day='+date});
+            });
+
+        $(".point").tooltip({
+            'container':'body',
+            'placement':'top'
+        });
+
+        var legend = svg.selectAll(".legend")
+            .data(cats.map(function(d) { return d.name; }))
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function (d, i) { return "translate(-25," + (i * 20 + 50) + ")"; });
+
+        legend.append("rect")
+            .attr("x", width - 10)
+            .attr("width", 10)
+            .attr("height", 10)
+            .style("fill", color)
+            .style("stroke", "grey");
+
+        legend.append("text")
+            .attr("x", width - 15)
+            .attr("y", 6)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(function (d) { return d; });
 });
 
 
